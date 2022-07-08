87813e5aad0351ec134c1557a3f923c6d260ddfc
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-07-08 11:35:56 -0300

    Remove configurations unused

--- next.config.js
+++ next.config.js
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  env: {
-    customKey: 'customValue',
-  },
-  // basePath: '/dist',
-  // compress: true,
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/hola',
-  //       destination: '/hello',
-  //       permanent: true,
-  //     },
-  //   ];
-  // },
 };
