f3fd9835e08d06fae228983da0e31eed7d27a26b
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-07-07 21:32:52 -0300

    Customizing next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  env: {
+    customKey: 'customValue',
+  },
+  // basePath: '/dist',
+  // compress: true,
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/hola',
+  //       destination: '/hello',
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
 };
