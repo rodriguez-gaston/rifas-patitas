c747e01f0b72a8e0f193f5a035ffef688726f03e
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-07-11 17:15:29 -0300

    Include Donation Sticky button

--- layouts/DefaultLayout.tsx
+++ layouts/DefaultLayout.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react';
 import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
+import DonationSticky from '../components/donation/DonationSticky';
 
 interface Props {
   children: ReactNode;
@@ -11,6 +12,7 @@ const DefaultLayout = ({ children }: Props) => {
     <>
       <Header />
       <main>{children}</main>
+      <DonationSticky />
       <Footer />
     </>
   );
