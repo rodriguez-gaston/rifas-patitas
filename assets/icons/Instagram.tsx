e87de4aaaf96b4ae5999ccdbefabe93149b85024
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-07-11 17:18:45 -0300

    Instagram link

---
+++ assets/icons/Instagram.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Instagram = () => {
+  return (
+    <a href="https://instagram.com/patitasenaccionok" target="_blank" rel="noreferrer">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="icon icon-tabler icon-tabler-brand-instagram"
+        width="28"
+        height="28"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <rect x="4" y="4" width="16" height="16" rx="4" />
+        <circle cx="12" cy="12" r="3" />
+        <line x1="16.5" y1="7.5" x2="16.5" y2="7.501" />
+      </svg>
+    </a>
+  );
+};
+
+export default Instagram;
