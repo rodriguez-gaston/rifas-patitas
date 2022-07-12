b77bf1a88126f0eff2a52c79611acb6af4ae32ce
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-07-12 11:46:05 -0300

    Change component to chakra

--- components/elements/donation/DonationSticky.tsx
+++ components/elements/donation/DonationSticky.tsx
@@ -1,10 +1,29 @@
 import Link from 'next/link';
 import React from 'react';
+import { Button } from '@chakra-ui/react';
 
 const DonationSticky = () => {
   return (
     <Link href={'/donar'}>
-      <button className="fixed bottom-5 right-4 bg-primary-700 text-white px-4 py-2 rounded-full hover:bg-primary-900">Donar</button>
+      <Button
+        pos="fixed"
+        bottom={5}
+        right={5}
+        borderRadius="full"
+        bg="primary.700"
+        color="white"
+        _hover={{
+          background: 'primary.800',
+          color: 'white',
+        }}
+        _active={{
+          background: 'primary.800',
+          color: 'white',
+        }}
+      >
+        Donar
+      </Button>
+      {/* <button className="fixed bottom-5 right-4 bg-primary-700 text-white px-4 py-2 rounded-full hover:bg-primary-900">Donar</button> */}
     </Link>
   );
 };
