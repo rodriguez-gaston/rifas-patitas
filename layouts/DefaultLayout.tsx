a15b19333e87c882fdbaf498e7509d24bbeaceed
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-07-12 12:29:57 -0300

    Change header import

--- layouts/DefaultLayout.tsx
+++ layouts/DefaultLayout.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react';
-import Header from '@/components/layout/Header';
+import Header from '@/components/layout/header/Header';
 import Footer from '@/components/layout/Footer';
 import DonationSticky from '@/components/elements/donation/DonationSticky';
 
