1ff7b8f10461dcdd59b07ab94d4523e7fa97b715
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-07-12 12:30:58 -0300

    Logo component

---
+++ components/layout/header/Logo.tsx
@@ -0,0 +1,14 @@
+import { Box, Text } from '@chakra-ui/react';
+import React from 'react';
+
+const Logo = () => {
+  return (
+    <Box>
+      <Text fontSize="lg" fontWeight="bold">
+        Patitas en Acción
+      </Text>
+    </Box>
+  );
+};
+
+export default Logo;
