c2cd7c065734ccd6a7de3224e0b1796c6a92be58
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-07-12 17:28:36 -0300

    Fix padding and margin

--- components/layout/header/NavBarContainer.tsx
+++ components/layout/header/NavBarContainer.tsx
@@ -7,7 +7,7 @@ interface NavBarProps {
 
 const NavBarContainer = ({ children, ...props }: NavBarProps) => {
   return (
-    <Flex as="nav" align="center" justify="space-between" wrap="wrap" w="100%" mb={8} p={8} shadow="sm" color="gray.800" {...props}>
+    <Flex as="nav" align="center" justify="space-between" wrap="wrap" w="100%" p={5} shadow="sm" color="gray.800" {...props}>
       {children}
     </Flex>
   );
