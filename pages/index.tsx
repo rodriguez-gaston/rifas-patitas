59499ddb3bd3160a65af35439bee94807268454c
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-07-12 11:46:44 -0300

    Remove tailwind classes

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div>
       <p>Hola Patitas</p>
     </div>
   );
