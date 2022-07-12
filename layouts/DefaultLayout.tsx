e4c820047c8b104e2cf3f0aa03f6998822aa6e6a
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-07-12 17:30:23 -0300

    Change import

--- layouts/DefaultLayout.tsx
+++ layouts/DefaultLayout.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from 'react';
 import Header from '@/components/layout/header/Header';
-import Footer from '@/components/layout/Footer';
+import Footer from '@/components/layout/footer/Footer';
 import DonationSticky from '@/components/elements/donation/DonationSticky';
 
 interface Props {
