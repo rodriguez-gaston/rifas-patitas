bc1b0976839a8bd1eed078c1cbbce457ce8ea481
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-07-12 12:30:14 -0300

    Menu icon

---
+++ assets/icons/Menu.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+const Menu = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="icon icon-tabler icon-tabler-menu"
+      width="28"
+      height="28"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <line x1="4" y1="8" x2="20" y2="8" />
+      <line x1="4" y1="16" x2="20" y2="16" />
+    </svg>
+  );
+};
+
+export default Menu;
