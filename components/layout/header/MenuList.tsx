5ca9b879c8bcf237b352f4fc8c9b646b01635878
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-07-12 12:31:23 -0300

    Menu list that include menu items

---
+++ components/layout/header/MenuList.tsx
@@ -0,0 +1,20 @@
+import { Box, Stack } from '@chakra-ui/react';
+import React from 'react';
+import MenuItem from './MenuItem';
+
+interface MenuListProps {
+  isOpen: boolean;
+}
+
+const MenuList = ({ isOpen }: MenuListProps) => {
+  return (
+    <Box display={{ base: isOpen ? 'block' : 'none', md: 'block' }} flexBasis={{ base: '100%', md: 'auto' }}>
+      <Stack spacing={8} align="center" justify={['center', 'center', 'flex-end', 'flex-end']} direction={['column', 'row', 'row', 'row']} pt={[4, 4, 0, 0]}>
+        <MenuItem to="/">Inicio</MenuItem>
+        <MenuItem to="/donar">Donar</MenuItem>
+      </Stack>
+    </Box>
+  );
+};
+
+export default MenuList;
