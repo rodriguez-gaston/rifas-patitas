d003e164a362f873cd997d24eac597967ac5141a
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-07-13 16:44:02 -0300

    Fix lint on navbar

--- components/layout/header/NavBarContainer.tsx
+++ components/layout/header/NavBarContainer.tsx
@@ -7,7 +7,17 @@ interface NavBarProps {
 
 const NavBarContainer = ({ children, ...props }: NavBarProps) => {
   return (
-    <Flex as="nav" align="center" justify="space-between" wrap="wrap" w="100%" p={5} shadow="sm" color="gray.800" {...props}>
+    <Flex
+      as="nav"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      w="100%"
+      p={5}
+      shadow="sm"
+      color="gray.800"
+      {...props}
+    >
       {children}
     </Flex>
   );
