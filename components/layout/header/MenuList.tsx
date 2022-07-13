7e31f7fd80591efcbf41268a7403c13696c06d99
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-07-13 16:44:19 -0300

    Fix lint on menu list

--- components/layout/header/MenuList.tsx
+++ components/layout/header/MenuList.tsx
@@ -8,8 +8,17 @@ interface MenuListProps {
 
 const MenuList = ({ isOpen }: MenuListProps) => {
   return (
-    <Box display={{ base: isOpen ? 'block' : 'none', md: 'block' }} flexBasis={{ base: '100%', md: 'auto' }}>
-      <Stack spacing={8} align="center" justify={['center', 'center', 'flex-end', 'flex-end']} direction={['column', 'row', 'row', 'row']} pt={[4, 4, 0, 0]}>
+    <Box
+      display={{ base: isOpen ? 'block' : 'none', md: 'block' }}
+      flexBasis={{ base: '100%', md: 'auto' }}
+    >
+      <Stack
+        spacing={8}
+        align="center"
+        justify={['center', 'center', 'flex-end', 'flex-end']}
+        direction={['column', 'row', 'row', 'row']}
+        pt={[4, 4, 0, 0]}
+      >
         <MenuItem to="/">Inicio</MenuItem>
         <MenuItem to="/donar">Donar</MenuItem>
       </Stack>
