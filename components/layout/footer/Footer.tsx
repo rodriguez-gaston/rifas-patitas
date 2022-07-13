d7386a37ba1fb078975c3d9b351958bac84982d5
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-07-13 16:46:12 -0300

    Add dynamic year

--- components/layout/footer/Footer.tsx
+++ components/layout/footer/Footer.tsx
@@ -3,6 +3,8 @@ import { Box, Stack, Center, SimpleGrid } from '@chakra-ui/react';
 import React from 'react';
 
 const Footer = () => {
+  const year = new Date().getFullYear();
+
   return (
     <footer>
       <Box p={5} borderTopWidth="thin" borderTopColor="gray.100">
@@ -14,8 +16,11 @@ const Footer = () => {
         </SimpleGrid>
       </Box>
       <Box px={5} py={3} bg="gray.200" color="gray.800">
-        <Stack direction={['column-reverse', 'row', 'row', 'row']} justify={['center', 'center', 'start', 'start']}>
-          <Center fontSize="sm">© 2022 Patitas en Acción</Center>
+        <Stack
+          direction={['column-reverse', 'row', 'row', 'row']}
+          justify={['center', 'center', 'start', 'start']}
+        >
+          <Center fontSize="sm">{`© ${year} Patitas en Acción`}</Center>
           <Center>
             <Instagram />
           </Center>
