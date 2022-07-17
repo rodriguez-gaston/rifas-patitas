51c79a2505460512f04ba3e2b5b0d8bad157dd52
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-07-16 23:55:45 -0300

    Add menu items to footer

--- components/layout/footer/Footer.tsx
+++ components/layout/footer/Footer.tsx
@@ -1,6 +1,14 @@
 import Instagram from '@/icons/Instagram';
-import { Box, Stack, Center, SimpleGrid } from '@chakra-ui/react';
+import {
+  Box,
+  Stack,
+  Center,
+  SimpleGrid,
+  Text,
+  Heading,
+} from '@chakra-ui/react';
 import React from 'react';
+import MenuItem from '@/components/layout/header/MenuItem';
 
 const Footer = () => {
   const year = new Date().getFullYear();
@@ -9,10 +17,15 @@ const Footer = () => {
     <footer>
       <Box p={5} borderTopWidth="thin" borderTopColor="gray.100">
         <SimpleGrid columns={[1, 1, 2, 4]} gap={6}>
-          <Box h="40" bg="blue.500" />
-          <Box h="40" bg="blue.500" />
-          <Box h="40" bg="blue.500" />
-          <Box h="40" bg="blue.500" />
+          <Box>
+            <Heading as="h5" size="sm">
+              Patitas en Acción
+            </Heading>
+            <Text>Cambiarles la vida esta en nostrxs</Text>
+          </Box>
+          <MenuItem to="/adoptar">Adoptar</MenuItem>
+          <MenuItem to="/ayudar">Ayudar</MenuItem>
+          <MenuItem to="/blog">Blog</MenuItem>
         </SimpleGrid>
       </Box>
       <Box px={5} py={3} bg="gray.200" color="gray.800">
