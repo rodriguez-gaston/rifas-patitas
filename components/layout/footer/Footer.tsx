b56fa3c009758dd818bb84ae277363046aef18c3
rodriguez-gaston <rodriguez.gaston.d@gmail.com>
2022-07-18 22:24:07 -0300

    Change footer layout and items

--- components/layout/footer/Footer.tsx
+++ components/layout/footer/Footer.tsx
@@ -1,43 +1,31 @@
 import Instagram from '@/icons/Instagram';
-import {
-  Box,
-  Stack,
-  Center,
-  SimpleGrid,
-  Text,
-  Heading,
-} from '@chakra-ui/react';
+import { Box, Text, Heading, Divider, Flex } from '@chakra-ui/react';
 import React from 'react';
-import MenuItem from '@/components/layout/header/MenuItem';
 
 const Footer = () => {
   const year = new Date().getFullYear();
 
   return (
     <footer>
-      <Box p={5} borderTopWidth="thin" borderTopColor="gray.100">
-        <SimpleGrid columns={[1, 1, 2, 4]} gap={6}>
-          <Box>
-            <Heading as="h5" size="sm">
-              Patitas en Acción
-            </Heading>
-            <Text>Cambiarles la vida esta en nostrxs</Text>
-          </Box>
-          <MenuItem to="/adoptar">Adoptar</MenuItem>
-          <MenuItem to="/ayudar">Ayudar</MenuItem>
-          <MenuItem to="/blog">Blog</MenuItem>
-        </SimpleGrid>
-      </Box>
-      <Box px={5} py={3} bg="gray.200" color="gray.800">
-        <Stack
-          direction={['column-reverse', 'row', 'row', 'row']}
-          justify={['center', 'center', 'start', 'start']}
-        >
-          <Center fontSize="sm">{`© ${year} Patitas en Acción`}</Center>
-          <Center>
+      <Box px={5} bg="gray.100" color="gray.800">
+        <Box py={5}>
+          <Flex gap={6} justifyContent="space-between">
+            <Box>
+              <Heading as="h5" size="sm">
+                Patitas en Acción
+              </Heading>
+              <Text fontSize="sm">Cambiarles la vida esta en nostrxs</Text>
+            </Box>
             <Instagram />
-          </Center>
-        </Stack>
+          </Flex>
+        </Box>
+        <Divider />
+        <Box py={5}>
+          <Text
+            textAlign={['center', 'center', 'left', 'left']}
+            fontSize="sm"
+          >{`© ${year} Patitas en Acción`}</Text>
+        </Box>
       </Box>
     </footer>
   );
